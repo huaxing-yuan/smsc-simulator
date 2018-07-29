671536a4ad09974cedac6f2b8a845305a1ca523b
Huaxing YUAN <newlucky@gmail.com>
2018-07-30 01:35:02 +0200

    update projet info

--- Hummingbird.Extension.SMSC/Properties/AssemblyInfo.cs
+++ Hummingbird.Extension.SMSC/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hummingbird.Extension.SMSC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SMSC simulator - An extension of Hummingbird Test Framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Huaxing YUAN")]
 [assembly: AssemblyProduct("Hummingbird.Extension.SMSC")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Huaxing YUAN, All rights reserved")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
